160a13772682d2e6dcc6bf45ada4bf2dc39cace0
rajaonahsoa <r.lalainafr@gmail.com>
2026-08-10 09:51:04 +0200

    config mailjet

--- base_app/views.py
+++ base_app/views.py
@@ -10,10 +10,10 @@ def index(request):
 def test_mail(request):
     
     send_mail(
-                'Dernier titre', # titre
-                "Dernier message", # message
-                settings.EMAIL_HOST_USER, # expediteur
-                ['r.lalainafr2@gmail.com'], # recipient
+                'Tester Un autre titre', # titre
+                " Tester Un autre message", # message
+                settings.DEFAULT_FROM_EMAIL, # expediteur
+                ['r.lalainafr@gmail.com'], # recipient
                 fail_silently=False
                 )
     return HttpResponse('Email sent')
